a480c10e415355d61f5f2ce036af2a02a85322be
Cathie So, PhD <socathie@gmail.com>
2022-09-19 13:57:52 +0000

    homework3

--- Homework/Homework03/Homework03.hs
+++ Homework/Homework03/Homework03.hs
@@ -4,6 +4,24 @@
 -- The function has to take the hourly consumption of an electrical device, the hours of daily use, and the maximum monthly consumption allowed.
 -- (Monthly usage = consumption (kW) * hours of daily use (h) * 30 days).
 
+checkConsumptionLet :: Double -> Double -> Double -> [Char]
+checkConsumptionLet consumption hours max =
+    let  monthlyConsumption = consumption * hours * 30
+    in
+        if monthlyConsumption > max
+            then "larger"
+            else
+                if monthlyConsumption < max
+                    then "smaller"
+                    else "equal"
+
+checkConsumptionWhere :: Double -> Double -> Double -> [Char]
+checkConsumptionWhere consumption hours max
+    | monthlyConsumption > max = "larger"
+    | monthlyConsumption < max = "smaller"
+    | True = "equal"
+    where 
+        monthlyConsumption = consumption * hours * 30
 
 -- Question 2
 -- Prelude:
@@ -11,6 +29,14 @@
 -- So `show 3` will produce `"3"` and `show (3 > 2)` will produce `"True"`.
 
 -- In the previous function, return the excess/savings of consumption as part of the message.
+
+checkConsumptionDifference :: Double -> Double -> Double -> [Char]
+checkConsumptionDifference consumption hours max
+    | monthlyConsumption > max = "larger by " ++ show (monthlyConsumption - max)
+    | monthlyConsumption < max = "smaller by " ++ show (max - monthlyConsumption)
+    | True = "equal"
+    where 
+        monthlyConsumption = consumption * hours * 30
 
 
 -- Question 3
@@ -23,8 +49,30 @@
 -- Return the number as a string, and in case the divisor is 0, return a message why the division is not
 -- possible. To implement this function using both guards and if-then-else statements.  
 
+quotient :: Double -> Double -> [Char]
+quotient x y
+    | x > y = if x /= 0 then show (y/x) else "Dividing by zero is undefined"
+    | x < y = if y /= 0 then show (x/y) else "Dividing by zero is undefined"
+    | True = "Dividing by zero is undefined"
+
 
 -- Question 5
 -- Write a function that takes in two numbers and calculates the sum of squares for the product and quotient
 -- of those numbers. Write the function such that you use a where block inside a let expression and a
 -- let expression inside a where block. 
+
+f :: Double -> Double -> Double
+f x y = 
+    let
+        prod = x*y
+        quot = x/y
+    in 
+        sq prod + sq quot
+        where
+            sq x = x*x
+
+g :: Double -> Double -> Double
+g x y = 
+    let sqProd = prod * prod where prod = x * y
+    in sqProd + sqQuot
+    where sqQuot = let quot = x/y in quot*quot
